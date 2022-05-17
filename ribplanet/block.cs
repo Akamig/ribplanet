0af343a327e82342c56361e6ee36dd47568c811d
Akamig <akamig@posteo.net>
2022-05-17 14:20:33 +0900

    Added block scaffolding

---
+++ ribplanet/block.cs
@@ -0,0 +1,15 @@
+namespace Ribplanet;
+public sealed class Block
+{
+    public int Index { get; }
+    public int Difficulty { get; }
+    public DateTimeOffset Timestamp { get; }
+    //public Nonce Nonce { get; }
+    //public RewardBeneficiary { get; }
+    //public Hash? PreviousHash { get; }
+
+    // public Hash hash(){}
+    // public Blockserialization serialize(){}
+    // public bencode(){}
+    // public valideate{}
+}
