aa028575f1811dd53144891433a9b5343ef432ce
Akamig <akamig@posteo.net>
2022-05-17 19:12:46 +0900

    HashCash scaffolding

---
+++ ribplanet/hashcash.cs
@@ -0,0 +1,15 @@
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace Ribplanet.HashCash
+{
+    public struct Hash
+    {
+        public byte[] hash;
+    }
+    public struct Nonce
+    {
+        public byte[] nonce;
+    }
+
+
+}
