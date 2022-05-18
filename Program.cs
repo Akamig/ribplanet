5db9176556255dd596843d9c6ef59b2c8aa1bf0c
Akamig <akamig@posteo.net>
2022-05-18 13:59:42 +0900

    simplify namespace

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Libplanet!");
+int counter = 1;
+while (true)
+{
+    double answerBytesLength = 1 + Math.Floor(Math.Log2(counter) / 8);
+    counter += 1;
+    Console.WriteLine(answerBytesLength);
+}
--- ribplanet/hashcash.cs
+++ ribplanet/hashcash.cs
@@ -1,14 +1,38 @@
+using System;
 using Org.BouncyCastle.Crypto.Digests;
 
-namespace Ribplanet.HashCash
+namespace Ribplanet
 {
     public struct Hash
     {
         public byte[] hash;
+        /*public static bool hasLeadingZerobits(Hash digest, int bits)
+        {
+        }*/
+    }
+    public struct Stamp
+    {
+        public byte[] stamp { get; set; }
+        public Stamp(byte[] stamp)
+        {
+            this.stamp = stamp;
+        }
     }
     public struct Nonce
     {
         public byte[] nonce;
+
+        public Nonce answer(int difficulty)
+        {
+            int counter = 1;
+            while (true)
+            {
+                double answerBytesLength = 1 + Math.Floor(Math.Log2(counter) / 8);
+                counter += 1;
+                Console.WriteLine(answerBytesLength);
+            }
+
+        }
     }
 
 
