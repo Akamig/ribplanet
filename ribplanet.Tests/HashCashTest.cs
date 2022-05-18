f0ea41a4306fc2aa40b99e4d128c1f3dd696b781
Akamig <akamig@posteo.net>
2022-05-18 14:00:13 +0900

    Adding HashCash Tests

---
+++ ribplanet.Tests/HashCashTest.cs
@@ -0,0 +1,11 @@
+using Xunit;
+using Ribplanet;
+using Libplanet.Crypto;
+namespace ribplanet.Tests;
+
+public class HashCashTests
+{
+    public void answerBytesLengthTest(){
+        Nonce.answer(320);
+    }
+}
