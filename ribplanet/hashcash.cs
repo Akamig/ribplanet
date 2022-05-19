f8b1b1c32188ad41225551ca4263e08c22edceb0
Akamig <akamig@posteo.net>
2022-05-19 17:30:53 +0900

    HasLeadingZeroBits Implementation

--- ribplanet/hashcash.cs
+++ ribplanet/hashcash.cs
@@ -6,9 +6,25 @@ namespace Ribplanet
     public struct Hash
     {
         public byte[] hash;
-        /*public static bool hasLeadingZerobits(Hash digest, int bits)
+        public bool HasLeadingZerobits(int bits)
         {
-        }*/
+            int leadingBytes = (int) Math.Round(bits / 8d, 1);
+            int trailingBits = bits % 8;
+            for(int i = 0; i < leadingBytes; i++){
+                if(this.hash[i] != 0x00){
+                    return false;
+                }
+            }
+            if(trailingBits != 0){
+                if(hash.Length <= leadingBytes){
+                    return false;
+                }
+                Int32 mask = 0b1111_1111 << (8 - trailingBits) & 0xff;
+                return (!(bool)(mask & this.hash[leadingBytes]))
+            }
+            return true;
+
+        }
     }
     public struct Stamp
     {
@@ -22,14 +38,13 @@ namespace Ribplanet
     {
         public byte[] nonce;
 
-        public Nonce answer(int difficulty)
+        public Nonce Answer(int difficulty)
         {
             int counter = 1;
             while (true)
             {
                 double answerBytesLength = 1 + Math.Floor(Math.Log2(counter) / 8);
                 counter += 1;
-                Console.WriteLine(answerBytesLength);
             }
 
         }
