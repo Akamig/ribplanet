2f6f737eae2a814f6794781ba551e97bfd8c3fcd
Akamig <akamig@posteo.net>
2022-05-19 17:29:28 +0900

    Type Casting test

--- ribplanet-cli/Program.cs
+++ ribplanet-cli/Program.cs
@@ -2,7 +2,7 @@
 int counter = 1;
 while (true)
 {
-    double answerBytesLength = 1 + Math.Floor(Math.Log2(counter) / 8);
+     answerBytesLength = 1 + Math.Floor(Math.Log2(counter) / 8);
     counter += 1;
     Console.WriteLine(answerBytesLength);
 }
