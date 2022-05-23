d63d248ec3335a19171defcb10dab87e266ccbbd
Akamig <akamig@posteo.net>
2022-05-23 16:02:21 +0900

    TX Scaffolding

--- ribplanet/tx.cs
+++ ribplanet/tx.cs
@@ -1,21 +1,44 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
 using Ribplanet;
 using Libplanet.Crypto;
+using Bencodex;
+using Bencodex.Types;
 
 namespace Ribplanet
 {
-    public class Transaction
+    public sealed class Transaction : IEquatable<Transaction<T>>
+        where T : IAction, new()
     {
-        public Address Sender;
-        public PublicKey PublicKey;
-        public byte[] Signature;
-        public Address Recipient;
-        public IImmutableList<T> Actions;
-        public DateTimeOffset Timestamp;
-        public byte[] TxId;
 
-        public Transaction Create(PrivateKey privateKey, Address recipient, DateTimeOffset timestamp){
-            publicKey = privateKey.PublicKey;
+        public Transaction(
+            Address Sender;
+        PublicKey PublicKey;
+        byte[] Signature;
+        Address Recipient;
+        IActions<T> Actions;
+        DateTimeOffset Timestamp;
+        byte[] TxId;
+    ){
+        
+    }
+
+        public Transaction Create(PrivateKey privateKey, Address recipient, DateTimeOffset timestamp)
+        {
+            Codec codec = new Codec();
+            PublicKey publicKey = privateKey.PublicKey;
+            Transaction t = new Transaction(
+                this.Sender = Address.GetAddress(publicKey),
+                this.PublicKey = publicKey,
+                this.Signature = new byte[] { 0x00 },
+                this.Recipient = recipient,
+                //this.Actions = List<Action>
+                this.Timestamp = timestamp
+            );
+            byte[] encoded = codec.Encode(t);
+
         }
     }
 }
