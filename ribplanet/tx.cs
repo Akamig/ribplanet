2c4ff8ab52cc3442072fcd6c86595e2c23e2afb2
Akamig <akamig@posteo.net>
2022-05-24 14:16:58 +0900

    Horrible TX

--- ribplanet/tx.cs
+++ ribplanet/tx.cs
@@ -3,42 +3,56 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using Ribplanet;
+using System.Security.Cryptography;
 using Libplanet.Crypto;
+using Libplanet.Action;
 using Bencodex;
 using Bencodex.Types;
 
-namespace Ribplanet
+namespace Ribplanet.Tx
 {
     public sealed class Transaction : IEquatable<Transaction<T>>
         where T : IAction, new()
     {
 
-        public Transaction(
-            Address Sender;
-        PublicKey PublicKey;
-        byte[] Signature;
-        Address Recipient;
-        IActions<T> Actions;
-        DateTimeOffset Timestamp;
-        byte[] TxId;
-    ){
-        
-    }
+        public byte[] TxId;
+        public Address Sender;
+        public PublicKey PublicKey;
+        public byte[] Signature;
+        public Address Recipient;
+        public IEnumerable<IAction> Actions;
+        public DateTimeOffset Timestamp;
 
-        public Transaction Create(PrivateKey privateKey, Address recipient, DateTimeOffset timestamp)
+        public Transaction(byte[] txId, PublicKey publicKey, Address recipient, IEnumerable<IAction> actions)
         {
-            Codec codec = new Codec();
-            PublicKey publicKey = privateKey.PublicKey;
-            Transaction t = new Transaction(
-                this.Sender = Address.GetAddress(publicKey),
-                this.PublicKey = publicKey,
-                this.Signature = new byte[] { 0x00 },
-                this.Recipient = recipient,
-                //this.Actions = List<Action>
-                this.Timestamp = timestamp
-            );
-            byte[] encoded = codec.Encode(t);
-
-        }
+            this.Sender = Address.GetAddress(publicKey);
+            this.PublicKey = publicKey;
+            this.Signature = new byte[] { 0x00 };
+            this.Recipient = recipient;
+            this.Actions = actions;
+            this.Timestamp = DateTimeOffset.UtcNow;
+        
+            byte[] payload = Serialize();
+            Console.WriteLine(payload.ToString());
+            using SHA256 algo = SHA256.Create();
+            this.TxId = algo.ComputeHash(payload);
     }
+        private byte[] Serialize()
+        {
+            
+            var bdict = Bencodex.Types.Dictionary.Empty
+                .Add(nameof(Sender), Sender.ToString())
+                .Add(nameof(PublicKey), PublicKey.ToString())
+                .Add(nameof(Signature), Signature)
+                .Add(nameof(Recipient), Recipient.ToString())
+                //actions missing
+                .Add(nameof(Timestamp), Timestamp.ToString());
+
+            return new Bencodex.Codec().Encode(bdict);
+        }
+        public bool Equals(Transaction<T> other) => TxId.Equals(other?.TxId);
+        public override bool Equals(object obj) => obj is Transaction<T> other && Equals(other);
+
+    }
+
 }
