ed8cde899b1a3b578ac9286133d07bd088367bb1
Akamig <akamig@posteo.net>
2022-05-26 16:48:22 +0900

    Blockchain scaffolding

--- ribplanet/blockchain.cs
+++ ribplanet/blockchain.cs
@@ -1,7 +1,15 @@
-
-
+using Ribplanet;
+using Ribplanet.Tx;
+using Ribplanet.Blocks;
 
 namespace Ribplanet.Blockchain
 {
-        
+    public class Blockchain{
+        private IDictionary<BlockHash, Block<T>> _blocks;
+        private Block<T> _genesis;
+
+        public BlockChain(
+            
+        )
+    }
 }
